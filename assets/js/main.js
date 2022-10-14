46decf31e9120c9c8f06025de4af6f4abf08cdbe
maxirosanda <maxi_rosanda@hotmail.com>
2022-10-14 17:26:19 -0300

    cambios varios

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,16 +11,14 @@ $(document).ready(function(){
         autoplaySpeed: 3000,
         responsive: [
             {
-              breakpoint: 1172,
+              breakpoint: 1298,
               settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
-                infinite: true,
-                dots: true
               }
             },
             {
-              breakpoint: 890,
+              breakpoint: 974,
               settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2
@@ -35,5 +33,38 @@ $(document).ready(function(){
             }
           ]
         
+      });
+  });
+
+  $(document).ready(function(){
+    $('.navegador__lista').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        dots: false,
+        focusOnSelect: true,
+        centerMode: true,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 636,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                centerMode: true,
+              
+              }
+            },
+            {
+                breakpoint: 430,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                  centerMode: true,
+                
+                }
+              },
+            
+          ]
+       
       });
   });
